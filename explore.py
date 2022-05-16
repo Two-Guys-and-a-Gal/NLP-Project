baf08bc7afecc30135fe8b83bed61cbcc48d2056
rachelrobbinsmayhill <rachel.robbinsmayhill@gmail.com>
2022-05-16 16:28:20 -0500

    Vis Two

--- explore.py
+++ explore.py
@@ -8,13 +8,14 @@ import json
 import requests
 
 import wrangle
-import prepare as prep
+import prepare
 
 from env import github_token, github_username
 
 # import for statistical testing
 import scipy.stats as stats
 from wordcloud import WordCloud
+from collections import Counter
 
 from sklearn.model_selection import train_test_split
 
@@ -30,8 +31,8 @@ plt.rc('font', size=15)
 plt.style.use('seaborn-deep')
 
 df = pd.read_json("data.json")
-df = prep.prep_data(df)
-train, validate, test = prep.split_data(df)
+df = prepare.prep_data(df)
+train, validate, test = prepare.split_data(df)
 
 def show_counts_and_ratios(df, column):
     """
@@ -43,6 +44,54 @@ def show_counts_and_ratios(df, column):
     labels.columns = ['n', 'percent']
     labels
     return labels
+
+
+def get_common_unique_words(text, threshold=5):
+    """
+    Get common unique words in dataframe, aka words that occur in multiple readme's
+    a word must appear in at least threshold readmes to be considered a common word
+    """
+
+    words = text.split()
+    counter = Counter(words)
+    common_unique_words = [word for word, count in counter.items() if count >= threshold]
+    new_string = ' '.join(common_unique_words)
+    return new_string
+
+
+# combine all strings in more_clean where language is the same
+languages = train.groupby('language')['more_clean'].apply(lambda x: ' '.join(x)).reset_index()
+languages.rename(columns={'more_clean':'all_words'}, inplace=True)
+languages['unique_words'] = train.groupby('language')['unique_words'].apply(lambda x: ' '.join(x)).values
+languages['common_unique_words'] = languages.unique_words.apply(get_common_unique_words)
+languages['n_words'] = languages['all_words'].apply(lambda x: len(x.split()))
+languages['unique_word_count'] = languages['all_words'].apply(lambda x: len(set(x.split())))
+languages['mean_word_count'] = train.groupby('language')['word_count'].mean().values.round(1)
+languages['median_word_count'] = train.groupby('language')['word_count'].median().values.round(1)
+languages['most_common_word'] = languages['unique_words'].apply(lambda x: prepare.n_most_common_word(x))
+languages['2nd_most_common_word'] = languages['unique_words'].apply(lambda x: prepare.n_most_common_word(x,2))
+languages['3rd_most_common_word'] = languages['unique_words'].apply(lambda x: prepare.n_most_common_word(x,3))
+languages['4th_most_common_word'] = languages['unique_words'].apply(lambda x: prepare.n_most_common_word(x,4))
+languages['5th_most_common_word'] = languages['unique_words'].apply(lambda x: prepare.n_most_common_word(x,5))
+
+
+
+
+# makes sets of all words in all languages so no words are repeated
+html_set = set(languages[languages.language=='HTML'].common_unique_words.values[0].split())
+python_set = set(languages[languages.language=='Python'].common_unique_words.values[0].split())
+r_set = set(languages[languages.language=='R'].common_unique_words.values[0].split())
+other_set = set(languages[languages.language=='Other'].common_unique_words.values[0].split())
+#remove words found in other languages
+unique_to_html = " ".join(html_set - python_set - r_set - other_set)
+unique_to_python = " ".join(python_set - html_set - r_set - other_set)
+unique_to_r = " ".join(r_set - html_set - python_set - other_set)
+unique_to_other = " ".join(other_set - html_set - python_set - r_set)
+# make a series to add to the dataframe
+unique_to_lang = [unique_to_html, unique_to_other, unique_to_python, unique_to_r]
+languages['unique_to_language'] = unique_to_lang
+
+
 
 
 ## Create lists of words for each language category
@@ -69,7 +118,7 @@ word_counts = (pd.concat([all_freq, other_freq, python_freq, r_freq, html_freq],
                 .apply(lambda s: s.astype(int)))
 
 
-def vis_one():
+def vis_one_a():
     '''
     This funciton......
     '''
@@ -77,7 +126,7 @@ def vis_one():
     plt.title('Word Count for top 5 Most Frequent Overall Words')
     plt.show()
     
-def vis_two():
+def vis_one_b():
     (word_counts.sort_values('all', ascending=False)
      .head(10)
      .apply(lambda row: row/row['all'], axis = 1)
@@ -88,6 +137,24 @@ def vis_two():
 
     plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     plt.title('% of Top 10 Word Frequency by Language')
+    plt.show()
+    
+    
+def vis_two():
+    # graph number of words in unique_to_language
+    new_df= languages[['language','unique_to_language']]
+    #new_df.set_index('language', inplace=True)
+    new_df['unique_words'] = new_df['unique_to_language'].apply(lambda x: len(x.split()))
+    new_df.sort_values(by='unique_words', ascending=False, inplace=True)
+    new_df.plot.bar(x='language', y='unique_words')
+    plt.title('Number of Words Unique to each Language')
+    plt.legend().set_visible(False)
+    # determine figure size
+    plt.rc('figure', figsize=(20, 8))
+    # determine font size
+    plt.rc('font', size=15)
+    # determine style
+    plt.style.use('seaborn-deep')
     plt.show()
     
     
